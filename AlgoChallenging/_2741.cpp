6e06851168b59517830c52abacc7da0d82cacd72
ddamik <jjgorl@hanmail.net>
2017-03-07 15:08:52 +0900

    Baekjoon 단계별 문제 풀어보기( For문 )

---
+++ AlgoChallenging/_2741.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+
+int _2741() {
+
+	int n;
+	scanf("%d", &n);
+	
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
---
+++ AlgoChallenging/algo.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+	//	2741
+	/*
+		int n;
+		scanf("%d", &n);
+		for (int i = 1; i <= n; i++) {
+			printf("%d\n", i);
+		}
+	*/
+
+
+	//	2742
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i >= 1; i--) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
