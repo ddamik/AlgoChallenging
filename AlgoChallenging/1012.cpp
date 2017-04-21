41d354da2f1e6ff9f5ef31392c91539dd88d0d39
ddamik <jjgorl@hanmail.net>
2017-04-22 07:48:37 +0900

    boj. bfs

--- AlgoChallenging/1012.cpp
+++ AlgoChallenging/1012.cpp
@@ -12,48 +12,60 @@ typedef pair<int, int> P;
 
 int main() {
 
-	int n, m, k;
+
+	int t, n, m, k;
 	int x, y, xx, yy;
-
-
-	scanf("%d %d %d", &n, &m, &k);
-
-	for (int i = 0; i < k; i++) {
-		scanf("%d %d", &x, &y);
-		map[x][y] = 1;
-	}
-
-	queue<P> que;
 	int result = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j] == 1 && visited[i][j] == 0) {
-				que.push(P(i, j));
-				visited[i][j] = 1;
-				result++;
+
+	scanf("%d", &t);
+
+	while (t--) {
+
+		result = 0;
+		scanf("%d %d %d", &m, &n, &k);
+
+		for (int i = 0; i < k; i++) {
+			scanf("%d %d", &x, &y);
+			map[y][x] = 1;
+		}
+
+		queue<P> que;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 1 && visited[i][j] == 0) {
+					que.push(P(i, j));
+					visited[i][j] = 1;
+					result++;
+
+					while (que.size()) {
+						x = que.front().first;
+						y = que.front().second;
+						que.pop();
+
+						for (int i = 0; i < 4; i++) {
+							xx = x + dirX[i];
+							yy = y + dirY[i];
+
+							if (xx < 0 || yy < 0 || xx >= n || yy >= m) continue;
+							if (map[xx][yy] == 0) continue;
+							if (visited[xx][yy] == 0 || (visited[xx][yy] > visited[x][y] + 1)) {
+								que.push(P(xx, yy));
+								visited[xx][yy] = visited[x][y] + 1;
+							}
+						}
+					}	//	while
+				}	//	if
+			}	//	for
+		}	//	for
+
+		printf("%d\n", result);
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < m; j++) {
+				map[i][j] = 0;
+				visited[i][j] = 0;
 			}
 		}
 	}
-
-
-	while (que.size()) {
-		x = que.front().first;
-		y = que.front().second;
-		que.pop();
-
-		for (int i = 0; i < 4; i++) {
-			xx = x + dirX[i];
-			yy = y + dirY[i];
-
-			if (xx < 0 || yy < 0 || xx >= n || yy >= m) continue;
-			if (visited[xx][yy] == 0 || (visited[xx][yy] > visited[x][y] + 1)) {
-				que.push(P(xx, yy));
-				visited[xx][yy] = visited[x][y] + 1;
-			}
-		}
-	}
-
-	printf("%d", result);
-
 	return 0;
 }
